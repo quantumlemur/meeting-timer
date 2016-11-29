29673acf1c46588e7f9822fc11d27c64cb8ae2f8
quantumlemur <quantumlemur@gmail.com>
2016-11-29 14:54:24 -0500

    converted to moment timestamps

--- timer-control.js
+++ timer-control.js
@@ -1,24 +1,24 @@
 var ViewModel = function() {
 	var self = this;
 
-	var gutterWidth = 20
+	var gutterWidth = 15
 
-	var color = d3.scale.category20c();
-
-
+	var color = d3.scale.category20c()
 
 
-	self.rawData = ko.observableArray([
-			{"id":"a", "name":"lunch", "scheduledStart":"50400", "scheduledEnd":"51400", "actualStart":"50400", "actualEnd":"52400", "done":true, "active":false},
-			{"id":"b", "name":"meeting", "scheduledStart":"51400", "scheduledEnd":"52400", "actualStart":"52400", "actualEnd":"54400", "done":false, "active":true},
-			{"id":"c", "name":"another meeting", "scheduledStart":"52400", "scheduledEnd":"53400", "actualStart":"54400", "actualEnd":"56400", "done":false, "active":false},
-			{"id":"d", "name":"coffee break", "scheduledStart":"53400", "scheduledEnd":"54400", "actualStart":"56400", "actualEnd":"58400", "done":false, "active":false},
-			{"id":"e", "name":"networking", "scheduledStart":"54400", "scheduledEnd":"55400", "actualStart":"58400", "actualEnd":"62400", "done":false, "active":false},
-			{"id":"f", "name":"yet another meeting", "scheduledStart":"55400", "scheduledEnd":"56400", "actualStart":"62400", "actualEnd":"64400", "done":false, "active":false},
-			{"id":"g", "name":"watching paint dry", "scheduledStart":"56400", "scheduledEnd":"57400", "actualStart":"64400", "actualEnd":"66400", "done":false, "active":false},
-			{"id":"h", "name":"evaporating into thin air", "scheduledStart":"57400", "scheduledEnd":"58400", "actualStart":"66400", "actualEnd":"68400", "done":false, "active":false},
-			{"id":"i", "name":"unicycle practice", "scheduledStart":"58400", "scheduledEnd":"59400", "actualStart":"68400", "actualEnd":"70400", "done":false, "active":false}
-		])
+
+	// assumed to be sorted by scheduledStart
+	var data = [
+			{"id":"a", "scheduledStart":"2016-11-28T09:20:00", "scheduledEnd":"2016-11-28T10:20:00", "actualStart":"2016-11-28T09:20:00", "actualEnd":"2016-11-28T10:20:00", "done":true, "active":false, "name":"lunch"},
+			{"id":"b", "scheduledStart":"2016-11-28T10:20:00", "scheduledEnd":"2016-11-28T11:20:00", "actualStart":"2016-11-28T10:20:00", "actualEnd":"2016-11-28T11:20:00", "done":false, "active":true, "name":"meeting"},
+			{"id":"c", "scheduledStart":"2016-11-28T11:20:00", "scheduledEnd":"2016-11-28T12:20:00", "actualStart":"2016-11-28T11:20:00", "actualEnd":"2016-11-28T12:20:00", "done":false, "active":false, "name":"another meeting"},
+			{"id":"d", "scheduledStart":"2016-11-28T12:20:00", "scheduledEnd":"2016-11-28T13:20:00", "actualStart":"2016-11-28T12:20:00", "actualEnd":"2016-11-28T13:20:00", "done":false, "active":false, "name":"coffee break"},
+			{"id":"e", "scheduledStart":"2016-11-28T13:20:00", "scheduledEnd":"2016-11-28T14:20:00", "actualStart":"2016-11-28T13:20:00", "actualEnd":"2016-11-28T14:20:00", "done":false, "active":false, "name":"networking"},
+			{"id":"f", "scheduledStart":"2016-11-28T14:20:00", "scheduledEnd":"2016-11-28T15:20:00", "actualStart":"2016-11-28T14:20:00", "actualEnd":"2016-11-28T15:20:00", "done":false, "active":false, "name":"yet another meeting"},
+			{"id":"g", "scheduledStart":"2016-11-28T15:20:00", "scheduledEnd":"2016-11-28T16:20:00", "actualStart":"2016-11-28T15:20:00", "actualEnd":"2016-11-28T16:20:00", "done":false, "active":false, "name":"watching paint dry"},
+			{"id":"h", "scheduledStart":"2016-11-28T16:20:00", "scheduledEnd":"2016-11-28T17:20:00", "actualStart":"2016-11-28T16:20:00", "actualEnd":"2016-11-28T17:20:00", "done":false, "active":false, "name":"evaporating into thin air"},
+			{"id":"i", "scheduledStart":"2016-11-28T17:20:00", "scheduledEnd":"2016-11-28T18:20:00", "actualStart":"2016-11-28T17:20:00", "actualEnd":"2016-11-28T18:20:00", "done":false, "active":false, "name":"unicycle practice"}
+		]
 
 	// $.ajax({
 	// 	url: 'api.html',
@@ -31,28 +31,56 @@ var ViewModel = function() {
 	// })
 
 
+	var minTime = moment.min(moment(data[0].scheduledStart), moment(data[0].actualStart))
+	var maxTime = moment.max(moment(data[data.length-1].scheduledEnd), moment(data[data.length-1].actualEnd))
 
-	self.render = function() {
-		var data = self.rawData()
+	var minHour = minTime.startOf('hour')
+	var maxHour = maxTime.startOf('hour')
+
+	var timeScale = d3.scale.linear()
+		.domain([minTime.valueOf(), maxTime.valueOf()])
+		.range([1,99])
+
+
+	var timeOverlayMajor = []
+	var timeOverlayMinor = []
+
+	var stepTime = minHour
+	while (stepTime <= maxHour) {
+		timeOverlayMajor.push({
+			label: stepTime.format('hh:mm A'),
+			y: timeScale(stepTime.valueOf()) + '%'
+		})
+		// half hour
+		stepTime.add(30, 'minutes')
+		timeOverlayMinor.push({
+			label: stepTime.format('hh:mm A'),
+			y: timeScale(stepTime.valueOf()) + '%'
+		})
+		stepTime.add(30, 'minutes')
+	}
+
+
+	data.map(function(d) {
+		d.startTime = moment(d.scheduledStart)
+		d.endTime = moment(d.scheduledEnd)
+		d.actualStartTime = moment(d.actualStart)
+		d.actualEndTime = moment(d.actualEnd)
+
+		d.duration = d.actualEndTime.diff(d.actualStartTime)
+		d.yScheduled = timeScale(d.startTime.valueOf())
+		d.heightScheduled = timeScale(d.endTime.valueOf()) - timeScale(d.startTime.valueOf())
+		d.yActual = timeScale(d.actualStartTime.valueOf())
+		d.heightActual = timeScale(d.actualEndTime.valueOf()) - timeScale(d.actualStartTime.valueOf())
+	})
+
+
+
+
+	var render = function() {
 
 		var RofL = (100-gutterWidth)/2
 		var LofR = 100-RofL
-
-		var minTime = Math.min(data[0].scheduledStart, data[0].actualStart)
-		var maxTime = Math.max(data[data.length-1].scheduledEnd, data[data.length-1].actualEnd)
-
-		var timeScale = d3.scale.linear()
-			.domain([minTime, maxTime])
-			.range([1,99])
-
-		var minHour = Math.floor(minTime / 3600)
-		var maxHour = Math.floor(maxTime / 3600)
-
-		var timeOverlayMajor = []
-		for (var i=0; i<maxHour-minHour+1; i++) {
-			var hour = i + minHour
-			timeOverlayMajor.push({label: hour+":00", hour:hour*3600, y:timeScale(hour*3600)+"%"})
-		}
 
 
 		d3.select(".timelineDiv")
@@ -83,7 +111,7 @@ var ViewModel = function() {
 
 
 		var events = timelineSVG.selectAll('.event')
-				.data(self.rawData())
+				.data(data)
 			.enter()
 
 		// left
@@ -91,30 +119,30 @@ var ViewModel = function() {
 				.attr('class', 'event')
 				.attr('fill', function(d) { return color(d.name) })
 				.attr('x', '1%')
-				.attr('y', function(d) { return timeScale(d.scheduledStart)+'%' })
+				.attr('y', function(d) { return d.yScheduled+'%' })
 				.attr('width', RofL-2+"%")
-				.attr('height', function(d) { return timeScale(d.scheduledEnd) - timeScale(d.scheduledStart) + '%' })
+				.attr('height', function(d) { return d.heightScheduled+'%' })
 
 		events.append('text')
 				.attr('class', 'eventText')
 				.text(function(d) { return d.name })
 				.attr('x', '2%')
-				.attr('y', function(d) { return timeScale(d.scheduledStart)+2+'%' })
+				.attr('y', function(d) { return d.yScheduled+1+'%' })
 
 		// right
 		events.append('rect')
 				.attr('class', 'event')
 				.attr('fill', function(d) { return color(d.name) })
 				.attr('x', LofR+1+'%')
-				.attr('y', function(d) { return timeScale(d.actualStart)+'%' })
+				.attr('y', function(d) { return d.yActual+'%' })
 				.attr('width', RofL-2+"%")
-				.attr('height', function(d) { return timeScale(d.actualEnd) - timeScale(d.actualStart) + '%' })
+				.attr('height', function(d) { return d.heightActual+'%' })
 
 		events.append('text')
 				.attr('class', 'eventText')
 				.text(function(d) { return d.name })
 				.attr('x', LofR+2+'%')
-				.attr('y', function(d) { return timeScale(d.actualStart)+2+'%' })
+				.attr('y', function(d) { return d.yActual+1+'%' })
 
 
 
@@ -124,7 +152,6 @@ var ViewModel = function() {
 		var majorLines = timelineSVG.selectAll('.majorLine')
 				.data(timeOverlayMajor)
 			.enter()
-
 
 		majorLines.append('text')
 				.attr('class', 'majorLineText')
@@ -148,12 +175,33 @@ var ViewModel = function() {
 				.attr('x2', '100%')
 				.attr('y2', function(d) { return d.y })
 
+		// minor lines
+		var minorLines = timelineSVG.selectAll('.minorLine')
+				.data(timeOverlayMinor)
+			.enter()
+
+		// left lines
+		minorLines.append('line')
+				.attr('class', 'minorLine')
+				.attr('x1', '0%')
+				.attr('y1', function(d) { return d.y })
+				.attr('x2', RofL+1+"%")
+				.attr('y2', function(d) { return d.y })
+
+		// right lines
+		minorLines.append('line')
+				.attr('class', 'minorLine')
+				.attr('x1', LofR-1+"%")
+				.attr('y1', function(d) { return d.y })
+				.attr('x2', '100%')
+				.attr('y2', function(d) { return d.y })
+
 
 	}
 
 
 
-	self.render()
+	render()
 }
 
 ko.applyBindings(new ViewModel());
