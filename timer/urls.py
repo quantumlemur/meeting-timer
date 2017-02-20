66372279cedcf26f8ac8f31b4841d6d6929f10d7
quantumlemur <quantumlemur@gmail.com>
2017-02-20 16:12:59 -0500

    added cross-component link buttons

--- timer/urls.py
+++ timer/urls.py
@@ -5,9 +5,9 @@ from . import timerControl
 from . import scheduleControl
 
 urlpatterns = [
-    url(r'^control/([0-9a-zA-Z_]*)$', views.timerControl, name='timerControl'),
-    url(r'^scheduleEditor/([0-9a-zA-Z_]*)$', views.scheduleEditor, name='scheduleEditor'),
-    url(r'^timer/([0-9a-zA-Z_]*)$', views.timer, name='timer'),
+    url(r'^control/([0-9a-zA-Z_-]*)$', views.timerControl, name='timerControl'),
+    url(r'^scheduleEditor/([0-9a-zA-Z_-]*)$', views.scheduleEditor, name='scheduleEditor'),
+    url(r'^timer/([0-9a-zA-Z_-]*)$', views.timer, name='timer'),
     url(r'^currentTimerInfo', timerControl.currentTimerInfo, name='currentTimerInfo'),
     url(r'^updateCurrentTimer', timerControl.updateCurrentTimer, name='updateCurrentTimer'),
     url(r'^saveSchedule', scheduleControl.saveSchedule, name='saveSchedule'),
